0618d25c3cfa2a811fd57e571104a729b746116b
Richard Munson <rmunson@ovitz.us>
2022-07-02 18:01:46 -0400

    Added some enum value code analysis attributes
    Update Enums.cs

---
+++ CoreUtils/CodeAnalysis/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rem.CoreUtils.CodeAnalysis;
+
+/// <summary>
+/// Indicates that an enumeration-type property, field or return value is always a named, defined value
+/// of its type.
+/// </summary>
+[AttributeUsage(
+        AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.ReturnValue,
+        AllowMultiple = false,
+        Inherited = true)]
+public class NamedEnumAttribute : Attribute { }
+
+/// <summary>
+/// Indicates that an enumeration-type parameter should always be a named, defined value of its type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+public class NamedEnumParameterAttribute : Attribute { }
+
