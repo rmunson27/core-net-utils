2d5c40f645e914cc8fadb4d3faff94520ce2b31f
Richard Munson <rmunson@ovitz.us>
2022-07-02 18:23:41 -0400

    Added missing enum code analysis class

---
+++ CoreUtils/CodeAnalysis/Enumerables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.CoreUtils.CodeAnalysis;
+
+/// <summary>
+/// Specifies that the value of an enumerable property, field or return value is non-empty.
+/// </summary>
+[AttributeUsage(
+    AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.ReturnValue,
+    AllowMultiple = false,
+    Inherited = true)]
+public sealed class NonEmptyAttribute : Attribute { }
+
+/// <summary>
+/// Specifies that the value of an enumerable parameter should never be empty.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+public sealed class NonEmptyParameterAttribute : Attribute { }
