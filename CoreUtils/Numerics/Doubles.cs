48d4c7ca18450efa41c869bab39e8deb37ad346b
Richard Munson <rmunson@ovitz.us>
2022-07-02 22:12:15 -0400

    Added helper classes for doubles and floats

---
+++ CoreUtils/Numerics/Doubles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rem.CoreUtils.Numerics;
+
+/// <summary>
+/// Extension methods and static functionality for working with <see cref="double"/> values.
+/// </summary>
+public static class Doubles
+{
+    /// <summary>
+    /// Determines if the <see cref="double"/> value passed in is finite (not infinity or NaN).
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public static bool IsFinite(double d) => !IsNotFinite(d);
+
+    /// <summary>
+    /// Determines if the <see cref="double"/> value passed in is not finite (infinity or NaN).
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public static bool IsNotFinite(double d) => double.IsInfinity(d) || double.IsNaN(d);
+}
---
+++ CoreUtils/Numerics/Floats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rem.CoreUtils.Numerics;
+
+/// <summary>
+/// Extension methods and static functionality for working with <see cref="float"/> values.
+/// </summary>
+public static class Floats
+{
+    /// <summary>
+    /// Determines if the <see cref="float"/> value passed in is finite (not infinity or NaN).
+    /// </summary>
+    /// <param name="f"></param>
+    /// <returns></returns>
+    public static bool IsFinite(float f) => !IsNotFinite(f);
+
+    /// <summary>
+    /// Determines if the <see cref="float"/> value passed in is not finite (infinity or NaN).
+    /// </summary>
+    /// <param name="f"></param>
+    /// <returns></returns>
+    public static bool IsNotFinite(float f) => float.IsInfinity(f) || float.IsNaN(f);
+}
