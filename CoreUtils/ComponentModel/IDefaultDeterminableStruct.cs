897f653f226b9d97405fc0bea50df83dbbf0f0ed
Richard Munson <rmunson@ovitz.us>
2022-07-02 23:29:44 -0400

    Fixed a miswritten namespace in "IDefaultDeterminableStruct.cs"

--- CoreUtils/ComponentModel/IDefaultDeterminableStruct.cs
+++ CoreUtils/ComponentModel/IDefaultDeterminableStruct.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace Rem.CoreUtils.Structs;
+namespace Rem.CoreUtils.ComponentModel;
 
 /// <summary>
 /// An interface for structure types that can determine if they are the default value.
