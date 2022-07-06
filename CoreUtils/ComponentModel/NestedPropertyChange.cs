33ca06a84af3185d630cf02f27d27b301b17a451
Richard Munson <rmunson@ovitz.us>
2022-07-06 15:02:29 -0400

    Added a helper class for subscribing to property change events of generic types that may not be known at compile time

--- CoreUtils/ComponentModel/NestedPropertyChange.cs
+++ CoreUtils/ComponentModel/NestedPropertyChange.cs
@@ -7,6 +7,8 @@ using System.Runtime.CompilerServices;
 
 namespace Rem.CoreUtils.ComponentModel;
 
+using static GenericPropertyChange;
+
 #region Classes
 /// <summary>
 /// A base class for objects that notify of nested property changes.
@@ -341,6 +343,332 @@ public abstract class NestedObservableObject
         NestedPropertyChanged?.Invoke(this, e);
     }
     #endregion
+}
+
+/// <summary>
+/// A class containing helper methods for subscribing to and unsubscribing from property change notification events
+/// implemented by a given generic reference type.
+/// </summary>
+/// 
+/// <remarks>
+/// This class can be useful when extending <see cref="NestedObservableObject"/> and attempting to yield accurate
+/// nested property change notifications on a generic reference type property that may not be known to implement
+/// property change notifications.
+/// </remarks>
+/// 
+/// <typeparam name="TNotifier">
+/// The generic reference type that potentially exposes property change events this class can be used to subscribe to
+/// and unsubscribe from.
+/// </typeparam>
+internal static class GenericPropertyChangeNotifier<TNotifier> where TNotifier : class
+{
+    private static readonly NotifierTypeDescription TNotifierTypeDescription;
+
+    static GenericPropertyChangeNotifier()
+    {
+        if (typeof(TNotifier).IsSubclassOf(typeof(NestedObservableObject))
+                || typeof(TNotifier) == typeof(NestedObservableObject))
+        {
+            TNotifierTypeDescription = NotifierTypeDescription.NestedObservableObject;
+        }
+        else // Need to look at the interfaces implemented by TNotifier
+        {
+            TNotifierTypeDescription = NotifierTypeDescription.None;
+            var tNotifierInterfaces = new HashSet<Type>(typeof(TNotifier).GetInterfaces());
+
+            if (tNotifierInterfaces.Contains(typeof(INotifyNestedPropertyChanging))
+                    || typeof(TNotifier) == typeof(INotifyNestedPropertyChanging))
+            {
+                TNotifierTypeDescription |= NotifierTypeDescription.NestedPropertyChanging;
+            }
+            else if (tNotifierInterfaces.Contains(typeof(INotifyPropertyChanging))
+                        || typeof(TNotifier) == typeof(INotifyPropertyChanging))
+            {
+                TNotifierTypeDescription |= NotifierTypeDescription.PropertyChanging;
+            }
+
+            if (tNotifierInterfaces.Contains(typeof(INotifyNestedPropertyChanged))
+                    || typeof(TNotifier) == typeof(INotifyNestedPropertyChanged))
+            {
+                TNotifierTypeDescription |= NotifierTypeDescription.NestedPropertyChanged;
+            }
+            else if (tNotifierInterfaces.Contains(typeof(INotifyPropertyChanged))
+                        || typeof(TNotifier) == typeof(INotifyPropertyChanged))
+            {
+                TNotifierTypeDescription |= NotifierTypeDescription.PropertyChanged;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Subscribes to any relevant property change events on the <typeparamref name="TNotifier"/> value passed in.
+    /// </summary>
+    /// 
+    /// <param name="value"></param>
+    /// <param name="propertyChanging"></param>
+    /// <param name="propertyChanged"></param>
+    /// <param name="nestedPropertyChanging"></param>
+    /// <param name="nestedPropertyChanged"></param>
+    /// <param name="ignoreSingularIfNested">
+    /// Whether or not to ignore singular (<see cref="INotifyPropertyChanging"/>, <see cref="INotifyPropertyChanged"/>)
+    /// interfaces when subscribing if their nested analogs are implemented.
+    /// </param>
+    public static void SubscribeTo(
+        TNotifier? value,
+        PropertyChangingEventHandler? propertyChanging,
+        PropertyChangedEventHandler? propertyChanged,
+        NestedPropertyChangingEventHandler? nestedPropertyChanging,
+        NestedPropertyChangedEventHandler? nestedPropertyChanged,
+        bool ignoreSingularIfNested)
+    {
+        if (value is not null)
+        {
+            if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.NestedPropertyChanging))
+            {
+                Unsafe.As<INotifyNestedPropertyChanging>(value).NestedPropertyChanging += nestedPropertyChanging;
+                if (!ignoreSingularIfNested)
+                {
+                    Unsafe.As<INotifyPropertyChanging>(value).PropertyChanging += propertyChanging;
+                }
+            }
+            else if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.PropertyChanging))
+            {
+                Unsafe.As<INotifyPropertyChanging>(value).PropertyChanging += propertyChanging;
+            }
+
+            if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.NestedPropertyChanged))
+            {
+                Unsafe.As<INotifyNestedPropertyChanged>(value).NestedPropertyChanged += nestedPropertyChanged;
+                if (!ignoreSingularIfNested)
+                {
+                    Unsafe.As<INotifyPropertyChanged>(value).PropertyChanged += propertyChanged;
+                }
+            }
+            else if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.PropertyChanged))
+            {
+                Unsafe.As<INotifyPropertyChanged>(value).PropertyChanged += propertyChanged;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Unsubscribes from any relevant property change events on the <typeparamref name="TNotifier"/> value passed in.
+    /// </summary>
+    /// 
+    /// <param name="value"></param>
+    /// <param name="propertyChanging"></param>
+    /// <param name="propertyChanged"></param>
+    /// <param name="nestedPropertyChanging"></param>
+    /// <param name="nestedPropertyChanged"></param>
+    public static void UnsubscribeFrom(
+        TNotifier? value,
+        PropertyChangingEventHandler? propertyChanging,
+        PropertyChangedEventHandler? propertyChanged,
+        NestedPropertyChangingEventHandler? nestedPropertyChanging,
+        NestedPropertyChangedEventHandler? nestedPropertyChanged)
+    {
+        if (value is not null)
+        {
+            if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.NestedPropertyChanging))
+            {
+                Unsafe.As<INotifyNestedPropertyChanging>(value).NestedPropertyChanging -= nestedPropertyChanging;
+                Unsafe.As<INotifyPropertyChanging>(value).PropertyChanging -= propertyChanging;
+            }
+            else if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.PropertyChanging))
+            {
+                Unsafe.As<INotifyPropertyChanging>(value).PropertyChanging -= propertyChanging;
+            }
+
+            if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.NestedPropertyChanged))
+            {
+                Unsafe.As<INotifyNestedPropertyChanged>(value).NestedPropertyChanged -= nestedPropertyChanged;
+                Unsafe.As<INotifyPropertyChanged>(value).PropertyChanged -= propertyChanged;
+            }
+            else if (TNotifierTypeDescription.HasDescriptionFlag(NotifierTypeDescription.PropertyChanged))
+            {
+                Unsafe.As<INotifyPropertyChanged>(value).PropertyChanged -= propertyChanged;
+            }
+        }
+    }
+}
+
+/// <summary>
+/// A class containing helper methods for subscribing to and unsubscribing from property change notification events
+/// implemented by a given generic reference type.
+/// </summary>
+/// 
+/// <remarks>
+/// This class can be useful when extending <see cref="NestedObservableObject"/> and attempting to yield accurate
+/// nested property change notifications on a generic reference type property that may not be known to implement
+/// property change notifications.
+/// </remarks>
+public static class GenericPropertyChange
+{
+    /// <summary>
+    /// Shuffles the events subscribed to from an old <typeparamref name="TNotifier"/> value to a new
+    /// <typeparamref name="TNotifier"/> value, according to the property change notifications implemented by the type.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// This method assumes that it is sufficient to subscribe to nested events rather than their singular
+    /// counterparts if possible; e.g. if <typeparamref name="TNotifier"/> implements
+    /// <see cref="INotifyNestedPropertyChanged"/>, then the method will subscribe to the
+    /// <see cref="INotifyNestedPropertyChanged.NestedPropertyChanged"/> event and ignore the
+    /// <see cref="INotifyPropertyChanged.PropertyChanged"/> event inherited by that interface.
+    /// </remarks>
+    /// 
+    /// <param name="oldValue"></param>
+    /// <param name="newValue"></param>
+    /// <param name="propertyChanging"></param>
+    /// <param name="propertyChanged"></param>
+    /// <param name="nestedPropertyChanging"></param>
+    /// <param name="nestedPropertyChanged"></param>
+    /// <param name="ignoreSingularIfNested">
+    /// Whether or not to ignore singular (<see cref="INotifyPropertyChanging"/>, <see cref="INotifyPropertyChanged"/>)
+    /// interfaces when subscribing if their nested analogs are implemented.
+    /// </param>
+    /// 
+    /// <typeparam name="TNotifier">
+    /// The generic reference type that potentially exposes property change events that this method can be used to
+    /// subscribe to and unsubscribe from.
+    /// </typeparam>
+    public static void ShuffleHandlers<TNotifier>(
+        TNotifier? oldValue, TNotifier? newValue,
+        PropertyChangingEventHandler? propertyChanging,
+        PropertyChangedEventHandler? propertyChanged,
+        NestedPropertyChangingEventHandler? nestedPropertyChanging,
+        NestedPropertyChangedEventHandler? nestedPropertyChanged,
+        bool ignoreSingularIfNested = true)
+        where TNotifier : class
+    {
+        // Unsubscribe from any relevant event handlers on the old value
+        UnsubscribeFrom(oldValue, propertyChanging, propertyChanged, nestedPropertyChanging, nestedPropertyChanged);
+
+        // Subscribe to any relevant event handlers on the new value
+        SubscribeTo(
+            newValue,
+            propertyChanging, propertyChanged, nestedPropertyChanging, nestedPropertyChanged,
+            ignoreSingularIfNested);
+    }
+
+    /// <summary>
+    /// Subscribes to any relevant property change events on the <typeparamref name="TNotifier"/> value passed in.
+    /// </summary>
+    /// 
+    /// <param name="value"></param>
+    /// <param name="propertyChanging"></param>
+    /// <param name="propertyChanged"></param>
+    /// <param name="nestedPropertyChanging"></param>
+    /// <param name="nestedPropertyChanged"></param>
+    /// <param name="ignoreSingularIfNested">
+    /// Whether or not to ignore singular (<see cref="INotifyPropertyChanging"/>, <see cref="INotifyPropertyChanged"/>)
+    /// interfaces when subscribing if their nested analogs are implemented.
+    /// </param>
+    /// 
+    /// <typeparam name="TNotifier">
+    /// The generic reference type that potentially exposes property change events that this method can be used to
+    /// subscribe to.
+    /// </typeparam>
+    public static void SubscribeTo<TNotifier>(
+        TNotifier? value,
+        PropertyChangingEventHandler? propertyChanging,
+        PropertyChangedEventHandler? propertyChanged,
+        NestedPropertyChangingEventHandler? nestedPropertyChanging,
+        NestedPropertyChangedEventHandler? nestedPropertyChanged,
+        bool ignoreSingularIfNested = true)
+        where TNotifier : class
+        => GenericPropertyChangeNotifier<TNotifier>.SubscribeTo(
+            value,
+            propertyChanging, propertyChanged, nestedPropertyChanging, nestedPropertyChanged,
+            ignoreSingularIfNested);
+
+    /// <summary>
+    /// Unsubscribes from any relevant property change events on the <typeparamref name="TNotifier"/> value passed in.
+    /// </summary>
+    /// 
+    /// <param name="value"></param>
+    /// <param name="propertyChanging"></param>
+    /// <param name="propertyChanged"></param>
+    /// <param name="nestedPropertyChanging"></param>
+    /// <param name="nestedPropertyChanged"></param>
+    /// 
+    /// <typeparam name="TNotifier">
+    /// The generic reference type that potentially exposes property change events that this method can be used to
+    /// unsubscribe from.
+    /// </typeparam>
+    public static void UnsubscribeFrom<TNotifier>(
+        TNotifier? value,
+        PropertyChangingEventHandler? propertyChanging,
+        PropertyChangedEventHandler? propertyChanged,
+        NestedPropertyChangingEventHandler? nestedPropertyChanging,
+        NestedPropertyChangedEventHandler? nestedPropertyChanged)
+        where TNotifier : class
+        => GenericPropertyChangeNotifier<TNotifier>.UnsubscribeFrom(
+            value,
+            propertyChanging, propertyChanged, nestedPropertyChanging, nestedPropertyChanged);
+
+    /// <summary>
+    /// Describes the type of property changes implemented by a given type.
+    /// </summary>
+    /// <remarks>
+    /// This enum respects the interface design of nested property change notifications; i.e. the
+    /// <see cref="NestedPropertyChanged"/> value has the <see cref="PropertyChanged"/> flag set because the
+    /// <see cref="INotifyNestedPropertyChanged"/> interface extends the <see cref="INotifyPropertyChanged"/>
+    /// interface.
+    /// </remarks>
+    internal enum NotifierTypeDescription : byte
+    {
+        /// <summary>
+        /// The type does not implement any property change notifications.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The type implements <see cref="INotifyPropertyChanging"/>.
+        /// </summary>
+        PropertyChanging = 1,
+
+        /// <summary>
+        /// The type implements <see cref="INotifyPropertyChanged"/>.
+        /// </summary>
+        PropertyChanged = 2,
+
+        /// <summary>
+        /// The type implements <see cref="INotifyNestedPropertyChanging"/> (and therefore
+        /// also <see cref="INotifyPropertyChanging"/>).
+        /// </summary>
+        NestedPropertyChanging = PropertyChanging | 4,
+
+        /// <summary>
+        /// The type implements <see cref="INotifyNestedPropertyChanged"/> (and therefore
+        /// also <see cref="INotifyPropertyChanged"/>).
+        /// </summary>
+        NestedPropertyChanged = PropertyChanged | 8,
+
+        /// <summary>
+        /// The type extends <see cref="NestedObservableObject"/>, and therefore implements
+        /// both <see cref="INotifyNestedPropertyChanging"/> and <see cref="INotifyNestedPropertyChanged"/>, and the
+        /// <see cref="ObservableObject.PropertyChanging"/> and <see cref="ObservableObject.PropertyChanged"/> events
+        /// are guaranteed to fire <see cref="NestedObservableObject.NestedPropertyChanging"/> and
+        /// <see cref="NestedObservableObject.NestedPropertyChanged"/> events, respectively.
+        /// </summary>
+        /// <remarks>
+        /// This is useful information, as a generic subscriber can subscribe to only the object's nested property
+        /// change handlers and trust that these will yield complete information about the changes happening in
+        /// the object.
+        /// </remarks>
+        NestedObservableObject = NestedPropertyChanging | NestedPropertyChanged | 16,
+    }
+
+    /// <summary>
+    /// A more efficient verison of <see cref="Enum.HasFlag(Enum)"/> for the
+    /// <see cref="NotifierTypeDescription"/> enum.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="flag"></param>
+    /// <returns></returns>
+    internal static bool HasDescriptionFlag(this NotifierTypeDescription value, NotifierTypeDescription flag)
+        => (value & flag) == flag;
 }
 #endregion
 
--- CoreUtilsTest/ComponentModel/NestedPropertyChangeTest.cs
+++ CoreUtilsTest/ComponentModel/NestedPropertyChangeTest.cs
@@ -13,9 +13,12 @@ namespace Rem.CoreUtilsTest.ComponentModel;
 [TestClass]
 public class NestedPropertyChangeTest
 {
+    #region Properties
     private ImmutableStack<string> ChangingPath { get; set; } = ImmutableStack<string>.Empty;
     private ImmutableStack<string> ChangedPath { get; set; } = ImmutableStack<string>.Empty;
+    #endregion
 
+    #region Tests
     /// <summary>
     /// Tests the nested property change events raised by instances of the <see cref="NestedObservableObject"/> class.
     /// </summary>
@@ -68,6 +71,159 @@ public class NestedPropertyChangeTest
         Assert.IsFalse(parentChange.ChangeImpliedBy(somethingElseChange.PropertyPath));
     }
 
+    /// <summary>
+    /// Tests the utility event subscription and unsubscription methods supplied by the
+    /// <see cref="GenericPropertyChange"/> class.
+    /// </summary>
+    [TestMethod]
+    public void TestGenericPropertyChange()
+    {
+        #region Variable Setup
+        const int changingFlag = 1;
+        const int changedFlag = 2;
+        const int nestedChangingFlag = 4;
+        const int nestedChangedFlag = 8;
+
+        int i;
+        #endregion
+
+        #region Tests
+        #region Changing Only
+        var changingOnly = new ChangingOnly();
+
+        i = 0;
+        SubscribeToNonNested(changingOnly);
+        changingOnly.I = 3;
+        Assert.AreEqual(changingFlag, i);
+        UnsubscribeFrom(changingOnly);
+        #endregion
+
+        #region Changed Only
+        var changedOnly = new ChangedOnly();
+
+        i = 0;
+        SubscribeToNonNested(changedOnly);
+        changedOnly.I = 3;
+        Assert.AreEqual(changedFlag, i);
+        UnsubscribeFrom(changedOnly);
+        #endregion
+
+        #region ObservableObject
+        var observable = new C();
+
+        i = 0;
+        SubscribeToNonNested(observable);
+        observable.BoolValue = true;
+        Assert.AreEqual(changingFlag | changedFlag, i);
+        UnsubscribeFrom(observable);
+        #endregion
+
+        #region Nested Changing Only
+        var nestedChangingOnly = new NestedChangingOnly();
+
+        i = 0;
+        SubscribeTo(nestedChangingOnly, true);
+        nestedChangingOnly.I = 3;
+        Assert.AreEqual(nestedChangingFlag, i);
+        UnsubscribeFrom(nestedChangingOnly);
+
+        i = 0;
+        SubscribeTo(nestedChangingOnly, false);
+        nestedChangingOnly.I = 2;
+        Assert.AreEqual(changingFlag | nestedChangingFlag, i);
+        UnsubscribeFrom(nestedChangingOnly);
+        #endregion
+
+        #region Nested Changed Only
+        var nestedChangedOnly = new NestedChangedOnly();
+
+        i = 0;
+        SubscribeTo(nestedChangedOnly, true);
+        nestedChangedOnly.I = 3;
+        Assert.AreEqual(nestedChangedFlag, i);
+        UnsubscribeFrom(nestedChangedOnly);
+
+        i = 0;
+        SubscribeTo(nestedChangedOnly, false);
+        nestedChangedOnly.I = 2;
+        Assert.AreEqual(changedFlag | nestedChangedFlag, i);
+        UnsubscribeFrom(nestedChangedOnly);
+        #endregion
+
+        #region ObservableObject Nested Changing Only
+        var observableNestedChangingOnly = new ObservableNestedChangingOnly();
+
+        i = 0;
+        SubscribeTo(observableNestedChangingOnly, true);
+        observableNestedChangingOnly.I = 4;
+        Assert.AreEqual(changedFlag | nestedChangingFlag, i);
+        UnsubscribeFrom(observableNestedChangingOnly);
+
+        i = 0;
+        SubscribeTo(observableNestedChangingOnly, false);
+        observableNestedChangingOnly.I = 5;
+        Assert.AreEqual(changingFlag | changedFlag | nestedChangingFlag, i);
+        UnsubscribeFrom(observableNestedChangingOnly);
+        #endregion
+
+        #region ObservableObject Nested Changed Only
+        var observableNestedChangedOnly = new ObservableNestedChangedOnly();
+
+        i = 0;
+        SubscribeTo(observableNestedChangedOnly, true);
+        observableNestedChangedOnly.I = 1;
+        Assert.AreEqual(changingFlag | nestedChangedFlag, i);
+        UnsubscribeFrom(observableNestedChangedOnly);
+
+        i = 0;
+        SubscribeTo(observableNestedChangedOnly, false);
+        observableNestedChangedOnly.I = 3;
+        Assert.AreEqual(changingFlag | changedFlag | nestedChangedFlag, i);
+        UnsubscribeFrom(observableNestedChangedOnly);
+        #endregion
+
+        #region NestedObservableObject
+        var nestedObservable = new A();
+
+        i = 0;
+        SubscribeTo(nestedObservable, true);
+        nestedObservable.BValue = new();
+        Assert.AreEqual(nestedChangingFlag | nestedChangedFlag, i);
+        UnsubscribeFrom(nestedObservable);
+
+        i = 0;
+        SubscribeTo(nestedObservable, false);
+        nestedObservable.BValue = new();
+        Assert.AreEqual(changingFlag | changedFlag | nestedChangingFlag | nestedChangedFlag, i);
+        UnsubscribeFrom(nestedObservable);
+        #endregion
+        #endregion
+
+        #region Methods
+        #region Helpers
+        void SubscribeToNonNested<T>(T value) where T : class
+            => SubscribeTo(value, true);
+
+        void SubscribeTo<T>(T value, bool ignoreSingularIfNested) where T : class
+            => GenericPropertyChange.SubscribeTo(
+                value, OnChanging, OnChanged, OnNestedChanging, OnNestedChanged, ignoreSingularIfNested);
+
+        void UnsubscribeFrom<T>(T value) where T : class
+            => GenericPropertyChange.UnsubscribeFrom(
+                value, OnChanging, OnChanged, OnNestedChanging, OnNestedChanged);
+        #endregion
+
+        #region Event Handlers
+        void OnChanging(object? _, PropertyChangingEventArgs e) { i |= changingFlag; }
+        void OnChanged(object? _, PropertyChangedEventArgs e) { i |= changedFlag; }
+        void OnNestedChanging(object? _, NestedPropertyChangingEventArgs e) { i |= nestedChangingFlag; }
+        void OnNestedChanged(object? _, NestedPropertyChangedEventArgs e) { i |= nestedChangedFlag; }
+        #endregion
+        #endregion
+    }
+    #endregion
+
+    #region Event Handlers
     private void ANestedChanging(object sender, NestedPropertyChangingEventArgs e)
     {
         ChangingPath = e.PropertyPath;
@@ -77,7 +233,9 @@ public class NestedPropertyChangeTest
     {
         ChangedPath = e.PropertyPath;
     }
+    #endregion
 
+    #region Classes
     private sealed class A : NestedObservableObject
     {
         public B? BValue
@@ -117,4 +275,109 @@ public class NestedPropertyChangeTest
         }
         private bool _boolValue;
     }
+
+    private sealed class ChangingOnly : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                PropertyChanging?.Invoke(this, new(nameof(I)));
+                _i = value;
+            }
+        }
+        private int _i;
+    }
+
+    private sealed class ChangedOnly : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                _i = value;
+                PropertyChanged?.Invoke(this, new(nameof(I)));
+            }
+        }
+        private int _i;
+    }
+
+    private sealed class NestedChangingOnly : INotifyNestedPropertyChanging
+    {
+        public event NestedPropertyChangingEventHandler? NestedPropertyChanging;
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                NestedPropertyChanging?.Invoke(this, new(nameof(I)));
+                PropertyChanging?.Invoke(this, new(nameof(I)));
+                _i = value;
+            }
+        }
+        private int _i;
+    }
+
+    private sealed class NestedChangedOnly : INotifyNestedPropertyChanged
+    {
+        public event NestedPropertyChangedEventHandler? NestedPropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                _i = value;
+                NestedPropertyChanged?.Invoke(this, new(nameof(I)));
+                PropertyChanged?.Invoke(this, new(nameof(I)));
+            }
+        }
+        private int _i;
+    }
+
+    private sealed class ObservableNestedChangingOnly : ObservableObject, INotifyNestedPropertyChanging
+    {
+        public event NestedPropertyChangingEventHandler? NestedPropertyChanging;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                NestedPropertyChanging?.Invoke(this, new(nameof(I)));
+                OnPropertyChanging();
+                _i = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _i;
+    }
+
+    private sealed class ObservableNestedChangedOnly : ObservableObject, INotifyNestedPropertyChanged
+    {
+        public event NestedPropertyChangedEventHandler? NestedPropertyChanged;
+
+        public int I
+        {
+            get => _i;
+            set
+            {
+                OnPropertyChanging();
+                _i = value;
+                OnPropertyChanged();
+                NestedPropertyChanged?.Invoke(this, new(nameof(I)));
+            }
+        }
+        private int _i;
+    }
+    #endregion
 }
