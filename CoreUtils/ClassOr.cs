0afc6ba1410729db027a11f3b51a162d8e62fc0e
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-10 19:31:11 -0400

    Removed an unnecessary `using` statement

--- CoreUtils/ClassOr.cs
+++ CoreUtils/ClassOr.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 using System.Text;
 
 namespace Rem.CoreUtils;
